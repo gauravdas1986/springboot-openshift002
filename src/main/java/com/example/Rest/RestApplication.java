a1a253bab7c7ccde57bba8e2773755e02c6aa624
729485 <729485@PC386399.cts.com>
2018-12-11 12:44:58 +0530

    camel integrated

--- src/main/java/com/example/Rest/RestApplication.java
+++ src/main/java/com/example/Rest/RestApplication.java
@@ -2,6 +2,8 @@ package com.example.Rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestApplication {
@@ -9,4 +11,13 @@ public class RestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
+	
+	 @Bean
+	    public ServletRegistrationBean servletRegistrationBean() {
+	        ServletRegistrationBean registration =
+	                new ServletRegistrationBean(new CamelHttpTransportServlet(), CAMEL_URL_MAPPING);
+	        registration.setName(CAMEL_SERVLET_NAME);
+
+	        return registration;
+	    }
 }
